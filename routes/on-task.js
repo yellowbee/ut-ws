bbe538690ddfebe1801cbb7a719fc0c515514c92
Ben Huang <yfenghuang@gmail.com>
2019-02-27 23:55:03 -0800

    fixes string to number conversion

--- routes/on-task.js
+++ routes/on-task.js
@@ -3,10 +3,11 @@ const Task = require("../models/task");
 const service = {
   getTasksWithFilterAndLimit: function(req, res) {
     let { industry, age, sex } = req.body;
+    console.log(req.params.limit);
     Task
       .find( { $and:[ {'industry':industry}, {'age':age}, {'sex':sex} ]} )
       .sort({'date': -1})
-      .limit(3)
+      .limit(parseInt(req.params.limit))
       .exec(function(err, posts) {
         res.json(posts);
       });
