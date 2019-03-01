9222ce5bce4475300d6646170f662a248eacca00
Ben Huang <yfenghuang@gmail.com>
2019-03-01 01:33:38 -0800

    adds endpoint to get tasks by filter and limit

--- routes/on-task.js
+++ routes/on-task.js
@@ -3,9 +3,22 @@ const Task = require("../models/task");
 const service = {
   getTasksWithFilterAndLimit: function(req, res) {
     let { industry, age, sex } = req.body;
-    console.log(req.params.limit);
+
+    let filter_arr = [];
+    if (industry && industry !== '不限') {
+      filter_arr.push({ $or:[ { industry: industry }, { industry: '不限'} ] });
+    }
+    if (age && age !== '不限') {
+      filter_arr.push({ $or:[ { age: age }, { age: '不限'} ] });
+    }
+    if (sex && sex !== '不限') {
+      filter_arr.push({ $or:[ { sex: sex }, { sex: '不限'} ] });
+    }
+
+    let filter_obj = filter_arr.length === 0 ? {} : { $and: filter_arr };
+
     Task
-      .find( { $and:[ {'industry':industry}, {'age':age}, {'sex':sex} ]} )
+      .find( filter_obj )
       .sort({'date': -1})
       .limit(parseInt(req.params.limit))
       .exec(function(err, posts) {
